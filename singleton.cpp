bc78041bdb9a1708bceca4fdacaaf79f2ff36244
wanhui <yao.wanhui@gmail.com>
2016-05-31 14:25:25 -0400

    Singleton base and singleton derived

--- singleton.cpp
+++ singleton.cpp
@@ -1,30 +1,84 @@
 #include "singleton.h"
 
-Singleton::Singleton():
-    v(0){
-    std::cout << __func__ << " is called" << std::endl;
+SingletonBase::SingletonBase():
+    id(0),
+className("SingletonBase"){
+    std::cout << className << "::" << __func__ << " is called" << std::endl;
 }
 
-Singleton& Singleton::instance(){
-    std::cout << __func__ << " is called" << std::endl;
-    static Singleton sing;
-    return sing;
+SingletonBase& SingletonBase::instance(){
+    std::cout << "SingletonBase" << "::" << __func__ << " is called" << std::endl;
+    static SingletonBase singRef;
+    return singRef;
 }
 
-Singleton::~Singleton(){
-    std::cout << __func__ << " is called" << std::endl;
+SingletonBase* SingletonBase::instancePoint(){
+    std::cout << "SingletonBase" << "::" << __func__ << " is called" << std::endl;
+    return &instance();
 }
 
-void Singleton::setValue(int v){
-    this->v = v;
+SingletonBase::~SingletonBase(){
+    std::cout << className << "::" << __func__ << " is called" << std::endl;
 }
 
-int Singleton::getValue() const{
-    return v;
+void SingletonBase::setId(int id){
+    this->id = id;
+}
+
+int SingletonBase::getId() const{
+    return id;
+}
+
+void SingletonBase::bar(){
+    std::cout << className << "::" << __func__ << " say hi" << std::endl;
+    std::cout << "\t id:" << id << std::endl;
+}
+
+
+void SingletonDerived::setValue(int v){
+    value = v;
+}
+
+void SingletonDerived::bar(){
+        std::cout << className << "::" << __func__ << " say hi" << std::endl;
+        std::cout << "\t id:" << id << " , value:" << value << std::endl;
+}
+
+SingletonDerived::SingletonDerived():
+    value(0),
+className("SingletonDerived"){
+
+}
+
+SingletonDerived& SingletonDerived::instance(){
+    std::cout << "SingletonDerived" << "::" << __func__ << " is called" << std::endl;
+    static SingletonDerived b;
+    return b;
+}
+
+SingletonDerived* SingletonDerived::instancePoint(){
+    std::cout << "SingletonDerived" << "::" << __func__ << " is called" << std::endl;
+    return &instance();
+}
+
+SingletonDerived::~SingletonDerived(){
+    std::cout << className << "::" << __func__ << " is called" << std::endl;
 }
 
 void testSingleton(){
+    SingletonBase::instance().bar();
+    SingletonBase::instance().setId(100);
+    SingletonBase* pSingleBase = SingletonBase::instancePoint();
+    pSingleBase->bar();
 
-    Singleton::instance().setValue(100);
-    std::cout << "value:" << Singleton::instance().getValue() << std::endl;
+    std::cout << "-------------------------" << std::endl;
+    SingletonDerived::instance().bar();
+    SingletonDerived::instance().setId(1000);
+    SingletonDerived::instance().setValue(2000);
+
+    SingletonDerived* pSingleDerived = SingletonDerived::instancePoint();
+    pSingleDerived->bar();
+
+    std::cout << "-------------------------" << std::endl;
+    SingletonBase::instancePoint()->bar();
 }
--- singleton.h
+++ singleton.h
@@ -1,17 +1,40 @@
 #ifndef SINGLETON_H
 #define SINGLETON_H
 #include <iostream>
-
-class Singleton
+#include <string>
+class SingletonBase
 {
-private:
-    Singleton();
-    ~Singleton();
-    int v;
 public:
-    void setValue(int v);
-    int getValue() const;
-    static Singleton& instance();
+    static SingletonBase& instance();
+    static SingletonBase* instancePoint();
+    void setId(int id);
+    int getId() const;
+    virtual void bar();
+    virtual ~SingletonBase();
+protected:
+    SingletonBase();
+    SingletonBase& operator=( const SingletonBase& ) = delete;
+    SingletonBase( const SingletonBase& other ) = delete;
+
+protected:
+    int id;
+private:
+    std::string className;
+};
+
+class SingletonDerived:public SingletonBase{
+public:
+    static SingletonDerived& instance();
+    static SingletonDerived* instancePoint();
+    void setValue(int);
+    virtual void bar();
+protected:
+    SingletonDerived();
+    virtual ~SingletonDerived();
+private:
+    int value;
+private:
+    std::string className;
 };
 
 void testSingleton();
